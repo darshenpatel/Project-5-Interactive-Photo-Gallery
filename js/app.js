b824de12138d974a0b72fcbf9e0378d4b7a9b1ae
Darshen <darshen.patel10@gmail.com>
2020-05-02 12:15:47 -0400

    Fifth Commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+let searchInput = document.getElementById('search-gallery');
+let imageContainer = document.getElementsByTagName('a');
+let captions = [];
+
 jQuery(document).ready(function($) {
     $('a[data-rel^=lightcase]').lightcase({
     transition: 'elastic',
@@ -6,13 +10,11 @@ jQuery(document).ready(function($) {
     });
 });
 
-let searchInput = document.getElementById('search-gallery');
+$('searchInput').on('click', function () {
+    $('placeholder').hide(); 
+}); 
 
-let imageContainer = document.getElementsByTagName('a');
-
-let captions = [];
-
-$('searchInput').keyup(function() {
+$('searchInput').keyup(function($) {
     let search = $('searchInput').val().toLowerCase(); 
     for (let i = 0; i < imageContainer.length; i++) {
         if (captions[i].indexOf(search) < 0) {
@@ -24,10 +26,7 @@ $('searchInput').keyup(function() {
     }
 });
 
-$('.imageContainer').each(function(index){
-    let dataCaption = $(this).children().attr('data-lc-caption').toLowerCase();
+$('imageContainer').each(function(index){
+    let dataCaption = $(this).children().attr('.images').toLowerCase();
     captions.push(dataCaption);
 });
-
-
-
