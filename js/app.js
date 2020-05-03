44437a9761eb6f3255a282b23e0c65899aae533a
Darshen <darshen.patel10@gmail.com>
2020-05-03 14:10:28 -0400

    Ninth Commit

--- js/app.js
+++ js/app.js
@@ -4,14 +4,17 @@ jQuery(document).ready(function($) {
         showSequenceInfo: false,
         showTitle: true, 
         transition: 'scrollHorizontal',
+        timeout: 3000, 
         transitionOpen: 'elastic',
         transitionClose: 'elastic'
+        
     });
 });
 
-// let input = $(document.getElementsByTagName('input'));
-// let imageContainer = $(document.querySelectorAll('a'));
+// let input = document.getElementById('search-gallery');
+// let imageContainer = document.getElementsByClassName('images');
 let captions = [];
+let resetSearch = document.getElementsByClassName('btn-reset');
 
 
 $('input').focus(function () {
@@ -22,8 +25,19 @@ $('input').blur(function () {
     $(this).attr('placeholder', 'Search');
 }); 
 
-$('.imageContainer').each(function () {
-    let dataCaption = $(this).attr('a').toLowerCase();
+$('button').mouseover(function () {
+    $(this).css('color', '#000');
+});
+
+$('button').mouseout(function () {
+    $(this).css('color', '#fff');
+});
+
+//With just .each(), the function selects a single parent element containing a single child. 
+//So when you call .each() on it, then it doesn't loop over them because it is not a collection of elements.
+$('.images').children().each(function (index) {
+    //This selects the attribute needed from each element and stores them individually in dataCaption:
+    let dataCaption = $(this).attr('data-lc-caption').toLowerCase();
     captions.push(dataCaption);
 });
 
@@ -31,14 +45,19 @@ $('input').keyup(function() {
     let searchFor = $(this).val().toLowerCase(); 
     for (let i = 0; i < captions.length; i++) {
         if (captions[i].indexOf(searchFor) < 0) {
-            $('.imageContainer').eq(i).hide();
+            //Having just imageContainer isn't the images nor is it the parent of the images.
+            //It's a single element that contains another element which is the parent of those images. 
+            //With a ('.images a') you target the parent element and children elements.
+            $('.images a').eq(i).hide();
         }
         else {
-            $('.imageContainer').eq(i).show(); 
+            $('.images a').eq(i).show(); 
         }
     }
 });
 
 
-
-
+$('.btn-reset').click(function () {
+    $('input').val('');
+    $('.images').attr('a');  
+}); 
